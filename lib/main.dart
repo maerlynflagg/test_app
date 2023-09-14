e624308dc29c13a77332d08fe33a6b6ec3cf3f4f
Thomas Riediger <riediger@myntag.onmicrosoft.com>
2023-09-14 14:00:40 +0200

    simple button

--- lib/main.dart
+++ lib/main.dart
@@ -114,47 +114,31 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> _init() async {
-    final bool succ = await _auth.authenticate(
-      localizedReason: 'Please do TouchID',
-      options: const AuthenticationOptions(
-        biometricOnly: true,
-        stickyAuth: true,
-      ),
-    );
-
-    if (succ) {
-       Navigator.of(context).pushNamed(SecondPage.routeName);
-    }
+    try {
+      final bool succ = await _auth.authenticate(
+        localizedReason: 'Please do TouchID',
+        options: const AuthenticationOptions(
+          biometricOnly: true,
+          stickyAuth: true,
+        ),
+      );
+    } catch (_) {}
   }
 
   Widget _buildBody() {
     return Center(
       // Center is a layout widget. It takes a single child and positions it
       // in the middle of the parent.
-      child: Column(
-        // Column is also a layout widget. It takes a list of children and
-        // arranges them vertically. By default, it sizes itself to fit its
-        // children horizontally, and tries to be as tall as its parent.
-        //
-        // Column has various properties to control how it sizes itself and
-        // how it positions its children. Here we use mainAxisAlignment to
-        // center the children vertically; the main axis here is the vertical
-        // axis because Columns are vertical (the cross axis would be
-        // horizontal).
-        //
-        // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-        // action in the IDE, or press "p" in the console), to see the
-        // wireframe for each widget.
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text(
-            'You have pushed the button this many times:',
-          ),
-          Text(
-            '$_counter',
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-        ],
+      child: ElevatedButton(
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(topRight: Radius.circular(18.0), bottomRight: Radius.circular(18.0)),
+          )),
+        ),
+        onPressed: () {
+          Navigator.of(context).pushNamed(SecondPage.routeName);
+        },
+        child: const Icon(Icons.camera_alt),
       ),
     );
   }
