c1b6fedd2ca2f17aff92cb0ae87b95fd1b1836da
Thomas Riediger <riediger@myntag.onmicrosoft.com>
2023-09-14 18:25:56 +0200

    extended pictture

---
+++ lib/image_model.dart
@@ -0,0 +1,15 @@
+
+class ImageModel {
+  
+  /// Thumbnail
+  late String thumbBase64;
+
+  /// base64 Daten
+  late String base64Image;
+
+    /// Konstruktor
+  ImageModel({
+    required this.base64Image,
+    required this.thumbBase64,
+  });
+}
--- lib/second_page.dart
+++ lib/second_page.dart
@@ -4,22 +4,64 @@ import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:image/image.dart' as _imgpackage;
+import './image_model.dart';
 
 class SecondPage extends StatefulWidget {
   static const routeName = '/second-page';
-  
-    const SecondPage({super.key});
+
+  const SecondPage({super.key});
 
   @override
   State<SecondPage> createState() => _SecondPageState();
 }
 
 class _SecondPageState extends State<SecondPage> {
-
   final ImagePicker _imagePicker = ImagePicker();
+
+  final List<ImageModel> _images = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final List<Widget> _childs = [];
+
+    for (ImageModel i in _images) {
+      _childs.add(Container(
+        margin: const EdgeInsets.fromLTRB(5.0, 0, 5.0, 2.0),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+          child: Stack(
+            children: <Widget>[
+              Image.memory(base64.decode(i.thumbBase64), fit: BoxFit.cover, height: 200),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  height: 30,
+                  width: double.infinity,
+                  color: Colors.black,
+                  child: const Center(
+                    child: Text('a pic', textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 10)),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ));
+    }
+
     return SafeArea(
       child: WillPopScope(
         onWillPop: () async => false,
@@ -38,18 +80,24 @@ class _SecondPageState extends State<SecondPage> {
             padding: const EdgeInsets.all(16.0),
             width: double.infinity,
             height: double.infinity,
-            child: Center(
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(topRight: Radius.circular(18.0), bottomRight: Radius.circular(18.0)),
-                  )),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      await _takePhoto();
+                    },
+                    child: const Icon(Icons.camera_alt),
+                  ),
                 ),
-                onPressed: () async {
-                  await _takePhoto();
-                },
-                child: const Icon(Icons.camera_alt),
-              ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: _childs,
+                  ),
+                ),
+              ],
             ),
           ),
         ),
@@ -59,18 +107,21 @@ class _SecondPageState extends State<SecondPage> {
 
   /// Foto aufnehmen
   Future<void> _takePhoto() async {
+    final XFile? _imageFile = await _imagePicker.pickImage(source: ImageSource.camera, maxHeight: 1024, maxWidth: 1024, imageQuality: 80);
 
-        final XFile? _imageFile = await _imagePicker.pickImage(source: ImageSource.camera, maxHeight: 1024, maxWidth: 1024, imageQuality: 80);
+    if (_imageFile != null) {
+      final File asFile = File(_imageFile.path);
+      final Uint8List imageBytes = await asFile.readAsBytes();
 
-        if (_imageFile != null) {
-          final File _asFile = File(_imageFile.path);
-          final Uint8List _imageBytes = await _asFile.readAsBytes();
+      final String asBase64 = base64.encode(imageBytes);
+      final _imgpackage.Image? image = _imgpackage.decodeImage(imageBytes);
+      final _imgpackage.Image thumbnail = _imgpackage.copyResize(image!, height: 200);
 
-          final String _asBase64 = base64.encode(_imageBytes);
-          await _asFile.delete();
+      await asFile.delete();
 
-
-          // do other stuff
-        }
-  }  
+      setState(() {
+        _images.add(ImageModel(base64Image: asBase64, thumbBase64: base64.encode(_imgpackage.encodeJpg(thumbnail))));
+      });
+    }
+  }
 }
